fc4dc20b8bf6ef8eda9495fc273c6ab387b562c1
daubie <daubie@localhost>
2020-05-14 19:20:42 +0200

    Page jeu

--- src/jeu.php
+++ src/jeu.php
@@ -4,11 +4,11 @@ require 'header.php';
 require 'nav.php';
 ?>
 
-<h1 class="title">Espace Log In</h1>
+<h1 class="title">Créer le quizz</h1>
 <h3 class="title2">Veuillez vous enregistrer</h3>
 
 <form action="/login.php" method="GET">
-ss
+
 </form>
 
 
