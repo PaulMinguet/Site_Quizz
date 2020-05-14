c362e0815746f767565c1fdc11d90fce52c6c842
daubie <daubie@localhost>
2020-05-14 19:18:29 +0200

    Page login

---
+++ src/jeu.php
@@ -0,0 +1,15 @@
+<?php 
+$title = "Quiz | LOGIN";
+require 'header.php';
+require 'nav.php';
+?>
+
+<h1 class="title">Espace Log In</h1>
+<h3 class="title2">Veuillez vous enregistrer</h3>
+
+<form action="/login.php" method="GET">
+ss
+</form>
+
+
+<?php require 'footer.php';?>
--- src/login.php
+++ src/login.php
@@ -7,8 +7,14 @@ require 'nav.php';
 <h1 class="title">Espace Log In</h1>
 <h3 class="title2">Veuillez vous enregistrer</h3>
 
-<form action="/login.php" method="GET">
-
+<form action="/login.php" method="post" class="login">
+    <label for="nom">Nom</label>
+    <input id="nom" type="text" name="nom" placeholder="Votre nom" class="area" />
+    <label for="prenom">Prénom</label>
+    <input id="prenom" type="text" name="prenom" placeholder="Votre prénom" class="area" />
+    <label for="email">Email</label>
+    <input id="email " type="email" name="email" placeholder="Votre adresse mail" class="area" />
+    <input type="submit" name="connect" value="Se connecter">
 </form>
 
 
