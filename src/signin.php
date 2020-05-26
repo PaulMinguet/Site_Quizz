46a10228bb43ad482e5acd42a698cc93ee0e1b97
daubie <daubie@localhost>
2020-05-26 19:49:27 +0200

    création statut

--- src/signin.php
+++ src/signin.php
@@ -16,14 +16,19 @@ require 'vues/msg_log.php';
     <input id="prenom" type="text" name="prenom" placeholder="Votre prénom" value="<?= htmlentities($prenom) ?>" class="area" />
     <label for="email">Email</label>
     <input id="email" type="email" name="email" placeholder="Votre adresse mail" value="<?= htmlentities($email) ?>" class="area" />
-    <label for="statut">Statut</label>
-    <select name="statut" id="statut" class="area">
-        <option value="Professeur">Professeur</option>
-        <option value="Elève">Elève</option>
-    </select>
     <label for="password">Mot de passe</label>
     <input id="password" type="text" name="password" placeholder="Votre mot de passe" value="<?= htmlentities($passwd) ?>" class="area" />
+    <label for="statut">Statut</label>
+    <div class="custom-select">
+        <select name="statut" id="statut">
+            <option value="0">Choisissez votre statut</option>
+            <option value="1">Professeur</option>
+            <option value="2">Élève</option>
+        </select>
+    </div>
     <input type="submit" name="connect" value="S'inscrire">
 </form>
 
 <?php require 'vues/footer.php';?>
+
+<script src="/js/btnSelect.js"></script>
