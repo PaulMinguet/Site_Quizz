87d47672eaf08e7ed7e34152d57c27975f47234c
daubie <daubie@localhost>
2020-05-28 20:35:16 +0200

    CI

--- CodeIgniter/application/controllers/home.php
+++ CodeIgniter/application/controllers/home.php
@@ -1,22 +1,24 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
-public function index() {
-        $this->accueil();
+    public function index() {
+        echo 'test';
     }
-    
-    public function accueil() {
+    public function accueil()
+    {
         $this->load->views('accueil');
     }
-    public function jeu() {
-        $this->load->views('jeu');
-    }
-    public function login() {
-        $this->load->views('login');
-    }
-    public function signin() {
-        $this->load->views('signin');
-    } 
+
+    // public function jeu() {
+    //     $this->load->views('jeu');
+    // }
+    // public function login() {
+    //     $this->load->views('login');
+    // }
+    // public function signin() {
+    //     $this->load->views('signin');
+    // } 
 }
 
 ?>
