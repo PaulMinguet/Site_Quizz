3cd85d5cf049f5736272708ade12d6c62fd2c7fb
daubie <daubie@localhost>
2020-06-02 16:10:28 +0200

    rien de spé

--- CodeIgniter/application/views/nav.php
+++ CodeIgniter/application/views/nav.php
@@ -4,6 +4,8 @@
         <li><a href="../home/accueil" id="nomnav"><?php if($_SESSION['username'] != null) echo "<span class='point_vert'></span>" . $_SESSION['username'];?></a></li>
         <li><a href="../home/signin" id="btn-sign"><i class="fas fa-sign-in-alt"></i>&nbsp;&nbsp;Sign In</a></li>
         <li><a href="../home/login" id="btn-log"><i class="fas fa-sign-in-alt"></i>&nbsp;&nbsp;Log In</a></li>
-        <?php if($_SESSION['username'] != null){echo "<li><a href='../home/deco' id='btn-dec'><i class='fas fa-sign-in-alt'></i>&nbsp;&nbsp;Déconnection</a></li>";}?>
+        <?php if($_SESSION['username'] != null){
+            echo "<li><a href='../home/deco' id='btn-dec'><i class='fas fa-sign-in-alt'></i>&nbsp;&nbsp;Déconnection</a></li>";}
+        ?>
     </ul>
 </nav>
