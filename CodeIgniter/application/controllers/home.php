6a28be07fa3474553495d440de73ff3f9305b85d
daubie <daubie@localhost>
2020-06-02 15:03:47 +0200

    Travail terminé

--- CodeIgniter/application/controllers/home.php
+++ CodeIgniter/application/controllers/home.php
@@ -12,6 +12,7 @@ class Home extends CI_Controller {
     	$this->load->view('header');
         $this->load->model('accueil');
         $this->load->view('accueil_view');
+        $this->load->view('cactus.php');
         $this->load->view('footer.php');
     }
     
@@ -20,10 +21,11 @@ class Home extends CI_Controller {
         $this->load->view('header');
         $this->load->model('jeu');
         $this->load->view('jeu_view');
+        $this->load->view('cactus.php');
         $this->load->view('questions');
         $this->load->view('footer.php');
     }
-
+    
     public function creer() {
         $this->load->view('header');
         $this->load->view('nav');
@@ -33,19 +35,21 @@ class Home extends CI_Controller {
         $this->load->view('creer_view');
         $this->load->view('footer');
     }
-
+    
     public function login() {
         $this->load->view('nav');
         $this->load->view('header');
         $this->load->model('login');
+        $this->load->view('cactus.php');
         $this->load->view('login_view');
         $this->load->view('footer.php');
     }
-
+    
     public function signin() {
         $this->load->view('nav');
         $this->load->view('header');
         $this->load->model('signin');
+        $this->load->view('cactus.php');
         $this->load->view('signin_view');
         $this->load->view('footer.php');
     }
