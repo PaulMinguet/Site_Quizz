0c2ef34006a1a0b944af0df05e2c84be35e9a671
daubie <daubie@localhost>
2020-06-03 20:59:55 +0200

    nav msg connect/deconnect

--- CodeIgniter/application/views/nav.php
+++ CodeIgniter/application/views/nav.php
@@ -1,7 +1,13 @@
 <nav>
     <ul>
         <li><a href="../home/accueil" id="btn-acc"><i class="fas fa-home"></i>&nbsp;&nbsp;Accueil</a></li>
-            <li><a href="../home/accueil" id="nomnav"><?php if(isset($_SESSION['username'])) echo "<span class='point_vert'></span>" . $_SESSION['username'];?></a></li>
+            <li><a href="../home/accueil" id="nomnav">
+            <?php if(isset($_SESSION['username'])){ 
+                    echo "<span class='point_co point_vert'></span>" . $_SESSION['username'];
+                } else {
+                    echo "<span class='point_co point_rouge'></span> <span>Veuillez vous connecter</span>";
+                }
+            ?></a></li>
         <li><a href="../home/signin" id="btn-sign"><i class="fas fa-sign-in-alt"></i>&nbsp;&nbsp;Sign In</a></li>
         <li><a href="../home/login" id="btn-log"><i class="fas fa-sign-in-alt"></i>&nbsp;&nbsp;Log In</a></li>
         <?php if(isset($_SESSION['username'])){echo "<li><a href='../home/deco' id='btn-dec'><i class='fas fa-sign-in-alt'></i>&nbsp;&nbsp;Déconnection</a></li>";}?>
