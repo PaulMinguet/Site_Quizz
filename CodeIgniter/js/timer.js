4c5b2c4bfb4b53e4f5f1bd0e4ff37535afe4b8b5
daubie <daubie@localhost>
2020-06-05 20:41:24 +0200

    Timer (pas fini)

--- CodeIgniter/js/timer.js
+++ CodeIgniter/js/timer.js
@@ -1,5 +1,69 @@
-const startHr = 23;  // Nbre d'heures de base
-const startMin = 60;  // Nbre de minutes de base
+const tempsmax = 6;   // valeur pour la bd
+let minutes = tempsmax - 1;
+let secondes = 60;
+let heures = 0;
+if (tempsmax > 60) {
+    heures = Math.trunc(tempsmax / 60);
+	minutes = tempsmax - heures * 60 - 1;
+}
+
+let timer = document.getElementById("timer");
+let refresh = setInterval(countDown, 1000);
+
+    if (heures < 10) {
+        heures = '0' + heures;
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    if (secondes < 10) {
+        secondes = '0' + secondes;
+    } 
+
+function countDown() {
+    secondes--;
+    if (secondes == 0) {
+        minutes--;
+    	secondes = 59;
+    }
+    if (minutes == 0) {
+        heures--;
+    	minutes = 59;
+    }else if ((heures == 0) && (minutes == 0) && (secondes == 0)) {
+        // alert("OVER !");
+        timer.innerHTML = "<span id='over' style='visibility:visible;'>OVER !</span>";
+        let over = document.getElementById('over');
+        clearInterval(refresh);
+    
+        function clignoter() {
+            if (over.style.visibility == 'visible') {
+                over.style.visibility = 'hidden';
+            } else {
+                over.style.visibility = 'visible';
+            }
+        }
+        periode = setInterval(clignoter, 400);
+    }
+   
+   
+    //// ----- Animation ----
+    if ((heures == 0) && (minutes < 10) && (minutes >= 5)) {
+        timer.style.color = '#2ECC72';
+        timer.style.transition = "all .5s";
+    } else if ((heures == 0) && (minutes < 5) && (minutes >= 2)) {
+        timer.style.color = '#ffe100';
+        timer.style.transition = "all .5s";
+    } else if ((heures == 0) && minutes < 2) {
+        timer.style.color = '#ff3845';
+        // timer.style.color = '#ffa200';
+        timer.style.transition = "all .5s";
+    }
+    display = `${heures}:${minutes}:${secondes}`;
+    timer.innerHTML = display;
+}
+
+/* const startHr = 23; // Nbre d'heures de base
+const startMin = 60; // Nbre de minutes de base
 let timehr = startHr * 60;
 let timemin = startMin * 60;
 
@@ -13,46 +77,48 @@ let timemin = startMin * 60;
 
 
 
-let refresh = setInterval(countDown, 1000);
 
 function countDown() {
     timemin--;
-    let hours = Math.floor(timehr / 60);
-    let minutes = Math.floor(timemin / 60);
-    let secondes = timemin % 60;
+	let heures = Math.floor(timehr / 60);
+	let minutes = Math.floor(timemin / 60);
+	let secondes = timemin % 60;
     let timer = document.getElementById("timer");
-    
-    secondes = secondes < 10 ? '0' + secondes : secondes;
-    
-    //// ----- Animation ----
-    if ((minutes < 10) && (minutes >= 5)) {
-        timer.style.color = '#2ECC72';
-        timer.style.transition ="all .5s";
-    } else if ((minutes < 5) && (minutes >= 2)) {
-        timer.style.color = '#ffe100';
-        timer.style.transition ="all .5s";
-    } else if(minutes < 2){
-        timer.style.color = '#ff3845';
-        // timer.style.color = '#ffa200';
-        timer.style.transition ="all .5s";
-    } 
-    display = `${hours}:${minutes}:${secondes}`;
-    timer.innerHTML = display;
-    if((minutes <= 0) && (secondes <= 0)){
-        timehr = 0;
-    }
-    if ((timehr === 0) && (timemin === 0)) {
-        // alert("OVER !");
-        timer.innerHTML = "<span id='over' style='visibility:visible;'>OVER !</span>";
-        let over = document.getElementById('over');
-        clearInterval(refresh);
-        function clignoter() {
-            if (over.style.visibility=='visible') {
-                over.style.visibility='hidden';
-            } else {
-                over.style.visibility='visible';
-            }
-        }
-        periode = setInterval(clignoter, 400);
-    }
-}
+
+	secondes = secondes < 10 ? '0' + secondes : secondes;
+	minutes = minutes < 10 ? '0' + minutes : minutes;
+	heures = heures < 10 ? '0' + heures : heures;
+
+	//// ----- Animation ----
+	if ((minutes < 10) && (minutes >= 5)) {
+		timer.style.color = '#2ECC72';
+		timer.style.transition = "all .5s";
+	} else if ((minutes < 5) && (minutes >= 2)) {
+		timer.style.color = '#ffe100';
+		timer.style.transition = "all .5s";
+	} else if (minutes < 2) {
+		timer.style.color = '#ff3845';
+		// timer.style.color = '#ffa200';
+		timer.style.transition = "all .5s";
+	}
+	display = `${heures}:${minutes}:${secondes}`;
+	timer.innerHTML = display;
+	if ((minutes <= 0) && (secondes <= 0)) {
+		timehr = 0;
+	}
+	if ((timehr === 0) && (timemin === 0)) {
+		// alert("OVER !");
+		timer.innerHTML = "<span id='over' style='visibility:visible;'>OVER !</span>";
+		let over = document.getElementById('over');
+		clearInterval(refresh);
+
+		function clignoter() {
+			if (over.style.visibility == 'visible') {
+				over.style.visibility = 'hidden';
+			} else {
+				over.style.visibility = 'visible';
+			}
+		}
+		periode = setInterval(clignoter, 400);
+	}
+} */
