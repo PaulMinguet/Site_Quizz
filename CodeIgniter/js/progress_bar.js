3c826253174910d487b1418ee78eb92fbd062bcc
daubie <daubie@localhost>
2020-06-08 17:48:12 +0200

    Accueil + timeline on scroll

--- CodeIgniter/js/progress_bar.js
+++ CodeIgniter/js/progress_bar.js
@@ -4,6 +4,7 @@ let totalHeight = document.body.scrollHeight - window.innerHeight;
 window.onscroll = function () {
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
     progress.style.height = progressHeight + "%";   
+    progress.style.transition = 'all .3s';
 }
 
 window.onload = function () {
