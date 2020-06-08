596a788687c29f1cfcbed4b99f91d162eb012a21
Paul <paul.minguet@etu.u-pec.fr>
2020-06-08 17:42:13 +0200

    Stats_bdd

--- CodeIgniter/application/models/auth.php
+++ CodeIgniter/application/models/auth.php
@@ -136,6 +136,20 @@
             }
         }
 
+        public function getIdUser(){
+            if(isset($_SESSION['email'])){                          //Si l'adresse email est renseignée
+                $builder = $this->db->select("utilisateur_id
+                                            FROM Utilisateur
+                                            WHERE utilisateur_mail = '".$_SESSION['email']."'", FALSE); //Alors on effectue une requête pour récupérer l'id de l'utilisateur
+                $query = $builder->get();
+                if($query->num_rows() > 0){                         //Si on trouve un résultat alors
+                    foreach ($query->result_array() as $row)
+                        $_SESSION['id'] = $row["utilisateur_id"];   //On assigne à la variable $_SESSION['id'] la valeur trouvée
+                }
+            }
+            return $_SESSION['id'];
+        }
+
         public function creer(){                                    //Fonction de création de quizz
             if(isset($this->nom_quizz)){
                 $this->load->model('fonctions');
@@ -146,7 +160,8 @@
                     'quizz_nom'         => $this->nom_quizz,
                     'quizz_cle'         => $this->codeAleatoire,
                     'quizz_duree'       => $this->duree,
-                    'quizz_nbQuestions' => $this->nb_questions
+                    'quizz_nbQuestions' => $this->nb_questions,
+                    'quizz_cree_par'    => $this->getIdUser()
                 );
                 $this->db->insert('Quizz', $data);
 
@@ -177,6 +192,82 @@
 
         public function stats() {
             # code...
+        }
+
+        public function getStatsQuizz(){
+            $returnHTML = "";
+            if(isset($_SESSION['email'])){                          //Si l'adresse email est renseignée
+                $builder = $this->db->select("quizz_id, quizz_nom, quizz_etat, quizz_cle, quizz_duree, quizz_nbQuestions
+                                            FROM Quizz
+                                            WHERE quizz_cree_par = ".$this->getIdUser(), FALSE); //Alors on effectue une requête pour récupérer le statut de l'utilisateur
+                $query = $builder->get();
+                if($query->num_rows() > 0){                         //Si on trouve un résultat alors
+                    $numQuizz = 1;
+                    foreach ($query->result_array() as $row){
+                        $hrs = (int) ($row["quizz_duree"]/3600);
+                        $min = (int) ($row["quizz_duree"]-$hrs*3600)/60;
+                        $sec = (int) ($row["quizz_duree"]-$hrs*3600-$min*60);
+                        $returnHTML = $returnHTML."<div class='container'>
+                            <div class='nbQuizz rep_nb'><h1>".$numQuizz."</h1></div>
+                            <div class='statutQuizz actif'>
+                                <h2><i class='fas fa-check-circle'></i>&nbsp;Actif</h2>
+                            </div>
+
+                            <div class='quizz'>
+                                <hr class='separate'/>
+                                <div class='line'>
+                                    <label for='nomqz'><span class='nb' id='nb1'>1</span> Nom du Quizz</label>
+                                    <div class='right_rep'>
+                                        <h1 class='nomquizz rep_text'>".$row["quizz_nom"]."</h1>
+                                    </div>
+                                </div>
+                                <div class='line'>
+                                    <label for='nomqz'><span class='nb' id='nb2'>2</span> Clé du Quizz</label>
+                                    <div class='right_rep'>
+                                        <h1 class='nomquizz rep_text'>".$row["quizz_cle"]."</h1>
+                                    </div>
+                                </div>
+                                <div class='line'>
+                                    <label for='nombre'><span class='nb' id='nb3'>3</span> Nombre de questions</label>
+                                    <div class='right_rep'>
+                                        <div class='rep_nb'>
+                                            <h1>".$row["quizz_nbQuestions"]."</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class='line'>
+                                    <label for='duree'><span id='nbStart'><i class='fas fa-stopwatch' id='nbStart2'></i></span> Durée</label>
+                                    <div class='right_rep'>
+                                        <div class='time-select-affich'>
+                                            <h1 name='hrs' id='hrs' class='tps hrs'>
+                                                ".$hrs."
+                                            </h1>
+                                            <span class='separ_tps'>:</span>
+                                            <h1 name='min' id='min' class='tps min'>
+                                                ".$min."
+                                            </h1>
+                                            <span class='separ_tps'>:</span>
+                                            <h1 name='sec' id='sec' class='tps sec'>
+                                                ".$sec."
+                                            </h1>
+                                            <!--<input type='time' name='durée' id='durée' min='12:00:00' max='18:00:00' required class='area_qz' onclick='colorizeNBStart()' required/> -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='bar_reussite'>
+                                <ul>
+                                    <li class='reussite'>Réussi</li>
+                                    <li class='moyen'>Moyen</li>
+                                    <li class='bof'>Bof Bof</li>
+                                </ul>
+                            </div>
+                        </div>";
+                        $numQuizz++;
+                    }
+                }
+            }
+            return $returnHTML;
         }
     }
 ?>
--- CodeIgniter/application/views/creer_view.php
+++ CodeIgniter/application/views/creer_view.php
@@ -2,7 +2,7 @@
     
     <h1 class="title">Créer le Quizz !</h1>
             
-    <form action="creer_question" method="post">
+    <form method="post">
         <div class="quizz">
             <hr class="separate"/>
             <div class="line">
@@ -62,7 +62,8 @@
         
 
         <div class='quizz' style='height: 150px; width: 500px'>
-            <button class='add' style='top: 8px; z-index: 100'>+<span>Ajouter une question</span></button>
+            <button class='add' style='top: 8px; z-index: 100;'>+</button>
+            <span class='add-span' style=" z-index: 200;">Ajouter un quizz</span>
             <hr class='separate'/>
             <br><br><br>
             <div class='final_btn'>
