0d6b1263059632241f908b4ab6c872db6d31cb83
Paul <paul.minguet@etu.u-pec.fr>
2020-06-10 18:34:08 +0200

    e

--- CodeIgniter/application/models/stats.php
+++ CodeIgniter/application/models/stats.php
@@ -6,8 +6,9 @@
 
 			$title = "Quiz | STATS QUIZZ";
 
-            $this->load->model('auth');		//Chargement du modele auth.php
-            $this->auth->stats();			//Chargement de la fonction creer()) de auth.php
+            $this->load->model('stats_bd');		//Chargement du modele stats_bd.php
+            $this->load->model('auth');
+            $this->stats_bd->getStatsQuizz();	//Chargement de la fonction getStatsQuizz()) de stats_bd.php
             
 		}
 	}
