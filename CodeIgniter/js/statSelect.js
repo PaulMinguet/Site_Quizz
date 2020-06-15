21aaa056c3fbe07410c8e32ebb811c825a16de67
Paul <paul.minguet@etu.u-pec.fr>
2020-06-15 18:16:46 +0200

    +stats

---
+++ CodeIgniter/js/statSelect.js
@@ -0,0 +1,26 @@
+/* 
+function onActif() {
+    
+    inactif.classList.remove("onSelect");
+}
+
+function onInactif() {
+    console.log("actif");
+    document.getElementById("inactif").classList.add("onSelect");
+    actif.classList.remove("onSelect");
+} */
+
+/* var actif = document.getElementById("actif")
+var inactif = document.getElementById("inactif")
+ */
+
+
+/* 
+ actif.onclick = function () {
+    actif.classList.add("onSelect");
+} 
+inactif.onclick = function () {
+    console.log("inactif");
+    actif.classList.add("onSelect");
+    
+} */
