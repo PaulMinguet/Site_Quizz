6856458decde36b81520a11f8e6f8d4ec0c65f72
Paul <paul.minguet@etu.u-pec.fr>
2020-06-16 23:49:15 +0200

    bugs

--- CodeIgniter/application/controllers/home.php
+++ CodeIgniter/application/controllers/home.php
@@ -8,102 +8,102 @@ class Home extends CI_Controller {
     }
     public function accueil()
     {
-        $this->load->view('Header');                //Chargement du Header : css, js, ...
-    	$this->load->view('Nav');                   //Chargement de la vue "Nav" : barre de Navigation
+        $this->load->view('header');                //Chargement du header : css, js, ...
+    	$this->load->view('nav');                   //Chargement de la vue "nav" : barre de navigation
         $this->load->model('Accueil');              //Chargement du model "accueil"
-        $this->load->view('Accueil_view');          //Chargement de la vue d'accueil
-        $this->load->view('Cactus');                //Chargement de la vue Cactus
-        $this->load->view('Footer');                //Chargement du Footer
-        $this->load->view('Progress_bar');          //Chargement de la barre de progression
+        $this->load->view('accueil_view');          //Chargement de la vue d'accueil
+        $this->load->view('cactus');                //Chargement de la vue cactus
+        $this->load->view('footer');                //Chargement du footer
+        $this->load->view('progress_bar');          //Chargement de la barre de progression
     }
     
     public function jeu() {                         //idem pour la page jeu
-        $this->load->view('Header');
-        $this->load->view('Nav');
-        $this->load->view('Timer');
+        $this->load->view('header');
+        $this->load->view('nav');
+        $this->load->view('timer');
         $this->load->model('Jeu');
-        $this->load->view('Jeu_view');
-        $this->load->view('Cactus');
-        $this->load->view('Footer');
-        $this->load->view('Progress_bar');
+        $this->load->view('jeu_view');
+        $this->load->view('cactus');
+        $this->load->view('footer');
+        $this->load->view('progress_bar');
     }
     public function note_eleve() {                         //idem pour la page note
-        $this->load->view('Header');
-        $this->load->view('Nav');
+        $this->load->view('header');
+        $this->load->view('nav');
         $this->load->model('Note_eleve');
-        $this->load->view('Note_eleve_view');
-        $this->load->view('Cactus');
-        $this->load->view('Footer');
-        $this->load->view('Progress_bar');
+        $this->load->view('note_eleve_view');
+        $this->load->view('cactus');
+        $this->load->view('footer');
+        $this->load->view('progress_bar');
     }
     
     public function creer() {                       //idem pour la page creer
-        $this->load->view('Header');
-        $this->load->view('Nav');
-        $this->load->view('Cactus');
+        $this->load->view('header');
+        $this->load->view('nav');
+        $this->load->view('cactus');
         $this->load->model('Creer');
-        $this->load->view('Creer_view');
-        $this->load->view('Footer');
-        $this->load->view('Progress_bar');
+        $this->load->view('creer_view');
+        $this->load->view('footer');
+        $this->load->view('progress_bar');
     }
     
     public function creer_question() {                       //idem pour la page creer question
-        $this->load->view('Header');
-        $this->load->view('Nav');
-        $this->load->view('Cactus');
+        $this->load->view('header');
+        $this->load->view('nav');
+        $this->load->view('cactus');
         $this->load->model('Creer_question');
-        $this->load->view('Creer_questions_view');
-        $this->load->view('Footer');
-        $this->load->view('Progress_bar');
+        $this->load->view('creer_questions_view');
+        $this->load->view('footer');
+        $this->load->view('progress_bar');
     }
 
     public function stats() {                       //idem pour la page statistiques
-        $this->load->view('Header');
-        $this->load->view('Nav');
-        $this->load->view('Cactus');
+        $this->load->view('header');
+        $this->load->view('nav');
+        $this->load->view('cactus');
         //$this->load->view('Timer');
         $this->load->model('Stats');
-        $this->load->view('Stats_view');
-        $this->load->view('Footer');
-        $this->load->view('Progress_bar');
+        $this->load->view('stats_view');
+        $this->load->view('footer');
+        $this->load->view('progress_bar');
     }
     
     public function login() {                       //idem pour la page login
-        $this->load->view('Header');
-        $this->load->view('Nav');
+        $this->load->view('header');
+        $this->load->view('nav');
         $this->load->model('Login');
-        $this->load->view('Cactus');
+        $this->load->view('cactus');
         $this->load->view('Login_view');
-        $this->load->view('Footer');
-        $this->load->view('Progress_bar');
+        $this->load->view('footer');
+        $this->load->view('progress_bar');
     }
 
     public function eleve_log() {                       //idem pour la page login élève
-        $this->load->view('Header');
-        $this->load->view('Nav');
+        $this->load->view('header');
+        $this->load->view('nav');
         $this->load->model('Eleve_log');
-        $this->load->view('Cactus');
-        $this->load->view('Eleve_log_view');
-        $this->load->view('Footer');
-        $this->load->view('Progress_bar');
+        $this->load->view('cactus');
+        $this->load->view('eleve_log_view');
+        $this->load->view('footer');
+        $this->load->view('progress_bar');
     }
     
     public function signin() {                      //idem pour la page signin
-        $this->load->view('Header');
-        $this->load->view('Nav');
+        $this->load->view('header');
+        $this->load->view('nav');
         $this->load->model('Signin');
-        $this->load->view('Cactus');
-        $this->load->view('Signin_view');
-        $this->load->view('Footer');
-        $this->load->view('Progress_bar');
+        $this->load->view('cactus');
+        $this->load->view('signin_view');
+        $this->load->view('footer');
+        $this->load->view('progress_bar');
     }
     
     public function deco() {                        //idem pour la page deco
-        $this->load->view('Header');
-        $this->load->view('Nav');
+        $this->load->view('header');
+        $this->load->view('nav');
         $this->load->model('Deco');
-        $this->load->view('Footer');
-        $this->load->view('Progress_bar');
+        $this->load->view('footer');
+        $this->load->view('progress_bar');
     }
 }
 
