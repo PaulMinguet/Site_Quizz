59c5ccc908189c57a7dff381b19cae6bd7b0d616
Paul <paul.minguet@etu.u-pec.fr>
2020-06-17 15:18:29 +0200

    timer

--- CodeIgniter/application/views/creer_view.php
+++ CodeIgniter/application/views/creer_view.php
@@ -66,7 +66,7 @@
         <div class='final_btn'>
             <div class='container' style='top: 50px;'>
                 <input type='submit' name='save' value='Enregistrer' class='save_btn' id='save_btn'>
-                <input type='submit' name='reset' value='Abandonner' class='reset_btn' id='reset_btn' onclick="<?php header('Location: ./Accueil')?>;">
+                <input type='submit' name='reset' value='Abandonner' class='reset_btn' id='reset_btn' onclick="URL='./Accueil'">
                 <script src='../../js/reload.js'></script>
             </div>
         </div>
