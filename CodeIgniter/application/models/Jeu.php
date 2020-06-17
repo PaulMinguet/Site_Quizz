50e5494c9ab3639e9f6c727129a867f0e10705a4
Paul <paul.minguet@etu.u-pec.fr>
2020-06-17 15:13:46 +0200

    timer

--- CodeIgniter/application/models/Jeu.php
+++ CodeIgniter/application/models/Jeu.php
@@ -7,8 +7,8 @@
 			$title = "Quiz | QUIZZ";
 
             $this->load->model('Jeu_bd');	//On charge tous les models et vues dont on a besoin pour la page de Jeu
-            //$this->load->model('Timer');
-            //$this->Timer->afficheTimer();
+            $this->load->model('Timer');
+            $this->Timer->afficheTimer();
             $this->load->model('Auth');
             $this->Auth->terminerQuizz();
             
