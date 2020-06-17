c005390bd37e1aa276a3bfe37b804dd798575b13
Paul <paul.minguet@etu.u-pec.fr>
2020-06-17 21:10:05 +0200

    timer

--- CodeIgniter/application/models/Timer.php
+++ CodeIgniter/application/models/Timer.php
@@ -20,43 +20,39 @@
                 }
             }
             if(!empty($hrs) || !empty($min) || !empty($sec)){
-                echo "
-                <!-- Element qui contiendra le timer -->
-                <div class='container-time'>
-			<p class='timer' id='demo timer'></p>
-		</div>
-		
-                <script>
-                // Definition du temps de demarrage
-                var countDownDate = new Date().getTime()+".$tot.";
+                echo "<!-- Display the countdown timer in an element -->
+            <p id="demo"></p>
 
-                // Maj ttes les secondes
-                var x = setInterval(function() {
+            <script>
+            // Set the date we're counting down to
+            var countDownDate = new Date().getTime()+".$tot.";
 
-                  // Recuperation de la date et l'hueure courante
-                  var now = new Date().getTime();
+            // Update the count down every 1 second
+            var x = setInterval(function() {
 
-                  // Calcul de la distance entre le temps de demarrage du chrono et l'heure de maintenant
-                  var distance = countDownDate - now;
+                // Get today's date and time
+                var now = new Date().getTime();
 
-                  // Calcul du temps pour les jours / heures / minutes / secondes
-                  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                // Find the distance between now and the count down date
+                var distance = countDownDate - now;
 
-                  // On affiche le resultat dans le h1 avec l'id 'demo'
-                  document.getElementById('demo').innerHTML = hours + ':'+ minutes + ':' + seconds;
+                // Time calculations for days, hours, minutes and seconds
+                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-                  // Une fois le timer a 0, on affiche EXPIRE
-                  if (distance < 0) {
+                // Display the result in the element with id="demo"
+                document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+                + minutes + "m " + seconds + "s ";
+
+                // If the count down is finished, write some text
+                if (distance < 0) {
                     clearInterval(x);
-                    document.getElementById('demo').innerHTML = 'EXPIRE';
-                    alert('Le quizz est expiré\nCeci est une attaque, toutes vos données ont été dérobées. Pour les récupérer, merci de verser 1.000.000 $ à Paul & à Louis\ndonc 2.000.000 $');
-                  }
-                }, 1000);
-
-                </script>";
+                    document.getElementById("demo").innerHTML = "EXPIRED";
+                }
+            }, 1000);
+            </script>";
             }
         }
     }
