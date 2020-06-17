7f7cc68c2e80564124a6dbc269b8e95026a73936
Paul <paul.minguet@etu.u-pec.fr>
2020-06-17 21:16:39 +0200

    timer

--- CodeIgniter/application/models/Timer.php
+++ CodeIgniter/application/models/Timer.php
@@ -25,7 +25,7 @@
 
             <script>
             // Set the date we're counting down to
-            var countDownDate = new Date().getTime()+'.$tot.';
+            var countDownDate = new Date().getTime()+".$tot.";
 
             // Update the count down every 1 second
             var x = setInterval(function() {
