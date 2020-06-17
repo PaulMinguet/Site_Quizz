a133bf6ccd4347d77d7112cc8aa3c23ed148c4fa
Paul <paul.minguet@etu.u-pec.fr>
2020-06-17 21:16:01 +0200

    timer

--- CodeIgniter/application/models/Timer.php
+++ CodeIgniter/application/models/Timer.php
@@ -21,11 +21,11 @@
             }
             if(!empty($hrs) || !empty($min) || !empty($sec)){
                 echo "<!-- Display the countdown timer in an element -->
-            <p id="demo"></p>
+            <p id='demo'></p>
 
             <script>
             // Set the date we're counting down to
-            var countDownDate = new Date().getTime()+".$tot.";
+            var countDownDate = new Date().getTime()+'.$tot.';
 
             // Update the count down every 1 second
             var x = setInterval(function() {
@@ -42,14 +42,14 @@
                 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
                 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-                // Display the result in the element with id="demo"
-                document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-                + minutes + "m " + seconds + "s ";
+                // Display the result in the element with id='demo'
+                document.getElementById('demo').innerHTML = days + 'd ' + hours + 'h '
+                + minutes + 'm ' + seconds + 's ';
 
                 // If the count down is finished, write some text
                 if (distance < 0) {
                     clearInterval(x);
-                    document.getElementById("demo").innerHTML = "EXPIRED";
+                    document.getElementById('demo').innerHTML = 'EXPIRED';
                 }
             }, 1000);
             </script>";
