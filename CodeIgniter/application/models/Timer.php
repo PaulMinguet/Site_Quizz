ccf5ccbf6f45655a83b878cc2bf3d95321212373
Paul <paul.minguet@etu.u-pec.fr>
2020-06-17 15:38:52 +0200

    timer

--- CodeIgniter/application/models/Timer.php
+++ CodeIgniter/application/models/Timer.php
@@ -14,10 +14,6 @@
             if($query->num_rows() > 0){                         //Si on trouve un résultat alors
                 foreach ($query->result_array() as $row){
                     $tot = $row["quizz_duree"]*1000;
-                    echo "temps tot : ".$tot."<br>";
-                    $hrs = (int) ($row["quizz_duree"]/3600);
-                    $min = (int) ($row["quizz_duree"]-$hrs*3600)/60;
-                    $sec = (int) ($row["quizz_duree"]-$hrs*3600-$min*60);
                 }
             }
             if(!empty($hrs) || !empty($min) || !empty($sec)){
@@ -39,19 +35,18 @@
                   var distance = countDownDate - now;
 
                   // Time calculations for days, hours, minutes and seconds
-                  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
                   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
                   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
                   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
                   // Display the result in the element with id='demo'
-                  document.getElementById('demo').innerHTML = days + 'd ' + hours + 'h '
+                  document.getElementById('demo').innerHTML = hours + 'h '
                   + minutes + 'm ' + seconds + 's ';
 
                   // If the count down is finished, write some text
                   if (distance < 0) {
                     clearInterval(x);
-                    document.getElementById('demo').innerHTML = 'EXPIRED';
+                    alert('Le quizz est expiré !');
                   }
                 }, 1000);
                 </script>";
