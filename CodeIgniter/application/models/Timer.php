4b211ca8e5e60d8d758325c23fca1c55209da6fb
Paul <paul.minguet@etu.u-pec.fr>
2020-06-17 21:27:24 +0200

    timer

--- CodeIgniter/application/models/Timer.php
+++ CodeIgniter/application/models/Timer.php
@@ -20,36 +20,36 @@
                 }
             }
             if(!empty($hrs) || !empty($min) || !empty($sec)){
-                echo "<!-- Afficher Le timer -->
-            <p id='timer'></p>
+                echo "<!-- Display the countdown timer in an element -->
+            <p id='demo'></p>
 
             <script>
-            // Initialisation de la date à partir de laquelle on décompte (ici, l'heure actuelle+la durée totale du quizz)
+            // Set the date we're counting down to
             var countDownDate = new Date().getTime()+".$tot.";
 
-            // Mise à jour du timer toute les secondes
+            // Update the count down every 1 second
             var x = setInterval(function() {
 
-                // On prend la date actuelle
+                // Get today's date and time
                 var now = new Date().getTime();
 
-                // On récupère la distance entre le temps choisi et maintenant
+                // Find the distance between now and the count down date
                 var distance = countDownDate - now;
 
-                // Calcul du temps en jours, heures, minutes et secondes
+                // Time calculations for days, hours, minutes and seconds
                 var days = Math.floor(distance / (1000 * 60 * 60 * 24));
                 var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
                 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
                 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-                // On affiche les résultats dans l'élément qui a pour id 'demo'
+                // Display the result in the element with id='demo'
                 document.getElementById('demo').innerHTML = days + 'd ' + hours + 'h '
                 + minutes + 'm ' + seconds + 's ';
 
-                // Lorsque le timer est terminé, on affiche une alerte
+                // If the count down is finished, write some text
                 if (distance < 0) {
                     clearInterval(x);
-                    document.getElementById('timer').innerHTML = 'EXPIRED';
+                    document.getElementById('demo').innerHTML = 'EXPIRED';
                 }
             }, 1000);
             </script>";
